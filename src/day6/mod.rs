04ec872353bebf51ad6b7ef131621d271071570e
Seweryn <sewerynpkopec@gmail.com>
2025-12-10 21:35:57 +0100

    [day6] solved part 2

--- src/day6/mod.rs
+++ src/day6/mod.rs
@@ -15,6 +15,14 @@ pub fn part1() {
     println!("Sum of all the operations: {}", sum);
 }
 
+pub fn part2() {
+    let lines = get_file_lines();
+    let input_grid = split_individual_operations(lines);
+    let results = calculate_factors_vertically(input_grid);
+    let sum: i64 = results.iter().sum();
+    println!("Sum of all the operations: {}", sum);
+}
+
 fn parse_lines(lines: Lines) -> Vec<Vec<String>> {
     let mut vec_of_lines = Vec::<Vec<String>>::new();
 
@@ -42,6 +50,33 @@ fn parse_lines(lines: Lines) -> Vec<Vec<String>> {
     return vec_of_lines;
 }
 
+fn split_individual_operations(lines: Lines) -> Vec<Vec<String>> {
+    let mut lines_vec = lines.collect::<Vec<&str>>();
+    let (operations, factors) = lines_vec.split_last_mut().expect("Invalid input");
+
+    let mut parsed_lines = vec![Vec::<String>::new(); factors.len() + 1];
+
+    let mut previous_split_index = 0;
+    for (index, character) in operations.char_indices() {
+        if character == ' ' || index == 0 {
+            continue;
+        }
+        let split_index = index - 1;
+        for (factor_line_index, factor_line) in factors.iter().enumerate() {
+            parsed_lines[factor_line_index].push(factor_line[previous_split_index..split_index].to_string());
+        }
+        parsed_lines[factors.len()].push(operations[previous_split_index..split_index].to_string());
+        // println!("operation {}", operations[previous_split_index..index].to_string());
+        previous_split_index = split_index + 1;
+    }
+    for (factor_line_index, factor_line) in factors.iter().enumerate() {
+        parsed_lines[factor_line_index].push(factor_line[previous_split_index..].to_string());
+    }
+    parsed_lines[factors.len()].push(operations[previous_split_index..].to_string());
+
+    parsed_lines
+}
+
 fn calculate_columns(input_grid: Vec<Vec<String>>) -> Vec<i64> {
     let mut results = Vec::<i64>::new();
     let (operations, factors) = &input_grid.split_last().unwrap();
@@ -58,14 +93,58 @@ fn calculate_columns(input_grid: Vec<Vec<String>>) -> Vec<i64> {
             },
             _ => None
         };
+        let operation = wrapped_operation.expect("Invalid input data for operation");
         for factor_row in factors.to_vec() {
             let current_factor: i64 = factor_row[col].as_str().parse().expect("Invalid input data for factors.");
             println!("{current_factor}");
-            let operation = wrapped_operation.expect("Invalid input data for operation");
 
             operation(&mut results[col], current_factor);
         }
     }
 
     return results;
+}
+
+fn calculate_factors_vertically(input_grid: Vec<Vec<String>>) -> Vec<i64> {
+    let mut results = Vec::<i64>::new();
+    let (operations, factors) = &input_grid.split_last().unwrap();
+    let factor_row_len = factors.len();
+    println!("operations len {}", operations.len());
+    println!("factors len {}", factors.first().unwrap().len());
+
+    let number_of_problems = input_grid.first().expect("Invalid input rows").len();
+    for problem in 0..number_of_problems  {
+        let current_operation_sign = &operations[problem].as_str().trim();
+        // println!("Current operation '{}'", current_operation_sign);
+        let wrapped_operation: Option<fn(&mut i64,i64)> = match *current_operation_sign {
+            "*" => {
+                results.push(1);
+                Some(|first, second| *first *= second)
+            },
+            "+" => {
+                results.push(0);
+                Some(|first, second| *first += second)
+            },
+            _ => None
+        };
+
+        let operation = wrapped_operation.expect("Invalid input data for operation");
+        let string_len = factors.first().unwrap()[problem].chars().count();
+
+        for char_position in 0..string_len {
+            let mut buffer = "".to_owned();
+            for factor_row in 0..factor_row_len {
+                let char_column: Vec<char> = factors[factor_row][problem].chars().collect();
+                // println!("char col {:?}", char_column.iter());
+                let digit = char_column[char_position];
+                // println!("Next digit {}", digit);
+                buffer.push(digit);
+            }
+            let parsed_number = buffer.trim().parse::<i64>().unwrap();
+            println!("Parsed number {}\n", parsed_number);
+            operation(&mut results[problem], parsed_number);
+        };
+    }
+
+    return results; 
 }
